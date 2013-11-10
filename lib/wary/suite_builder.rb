fcb1e984bdf7f9c7220ff8d8c9754092d4e08b6d
Robert Curth <robert@rocu.de>
2013-11-10 15:00:01 +0100

    Fixes bug with constantize for Ruby 1.9.3

--- lib/wary/suite_builder.rb
+++ lib/wary/suite_builder.rb
@@ -9,9 +9,22 @@ class Wary::SuiteBuilder
   def build
     initialized_checks = @checks.map do |name, config|
       klass = config.fetch(:class)
-      Kernel.const_get("Wary::Check::#{klass}").new(config)
+      config[:name] = name
+      constantize("Wary::Check::#{klass}").new(config)
     end
 
     Wary::CheckSuite.new(initialized_checks)
+  end
+
+  private
+  def constantize(camel_cased_word)
+    names = camel_cased_word.split('::')
+    names.shift if names.empty? || names.first.empty?
+
+    constant = Object
+    names.each do |name|
+      constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+    end
+    constant
   end
 end
